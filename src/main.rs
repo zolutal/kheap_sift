d29afe987df65e5d3fe6aa090f1be7af91659a8e
zolutal <justalmill@gmail.com>
2024-03-07 17:32:56 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use std::sync::{Arc, RwLock, Mutex};
 use std::collections::HashMap;
 use std::io::IsTerminal;
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex, RwLock};
 
 use dwat::prelude::*;
 
-use ts::{Parser as TsParser, Query, QueryCursor};
-use tree_sitter_c as ts_c;
 use tree_sitter as ts;
+use tree_sitter_c as ts_c;
+use ts::{Parser as TsParser, Query, QueryCursor};
 
-use lazy_static::lazy_static;
 use clap::ArgAction::Append;
-use tokio::io::AsyncReadExt;
-use tokio::sync::Semaphore;
-use tokio::fs::File;
+use clap::Parser;
+use lazy_static::lazy_static;
 use memmap2::Mmap;
 use regex::Regex;
-use clap::Parser;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use tokio::sync::Semaphore;
 use tokio::task;
 
 #[derive(Parser)]
@@ -38,8 +38,12 @@ struct CmdArgs {
     upper_bound: usize,
 
     /// Silence dwat/weggli output, only print struct names
-    #[clap(long, action, help = "Silence dwat/weggli output, only print struct \
-                                 names.")]
+    #[clap(
+        long,
+        action,
+        help = "Silence dwat/weggli output, only print struct \
+                                 names."
+    )]
     quiet: bool,
 
     /// Allocation flags flags argument regex
@@ -67,7 +71,12 @@ fn collect_src_files(dir: &PathBuf) -> Vec<PathBuf> {
         .into_iter()
         .filter_map(|e| e.ok())
         .map(|e| e.into_path())
-        .filter(|path| path.is_file() && path.extension().map_or(false, |ext| ext == "c" || ext == "h"))
+        .filter(|path| {
+            path.is_file()
+                && path
+                    .extension()
+                    .map_or(false, |ext| ext == "c" || ext == "h")
+        })
         .collect()
 }
 
@@ -82,22 +91,23 @@ async fn main() -> anyhow::Result<()> {
     let struct_map = dwarf.get_named_types_map::<dwat::Struct>()?;
 
     let struct_map: HashMap<String, dwat::Struct> = {
-        struct_map.into_iter().filter(|(_, struc)| {
-            if let Ok(bytesz) = struc.byte_size(&dwarf) {
-                args.lower_bound < bytesz && bytesz <= args.upper_bound
-            } else {
-                false
-            }
-        }).collect()
+        struct_map
+            .into_iter()
+            .filter(|(_, struc)| {
+                if let Ok(bytesz) = struc.byte_size(&dwarf) {
+                    args.lower_bound < bytesz && bytesz <= args.upper_bound
+                } else {
+                    false
+                }
+            })
+            .collect()
     };
 
     let iter_files: Vec<PathBuf> = collect_src_files(&args.source_path);
     let mut files: Vec<PathBuf> = vec![];
 
-
     let exclude_globs = &args.exclude;
     if exclude_globs.len() > 0 {
-
         let mut builder = globset::GlobSetBuilder::new();
         for glob in exclude_globs {
             builder.add(globset::Glob::new(glob)?);
@@ -136,7 +146,9 @@ async fn main() -> anyhow::Result<()> {
         let shared_dwarf = Arc::clone(&shared_dwarf);
         let flags_regex_str = args.flags.clone();
         let handle = tokio::spawn(async move {
-            read_and_process_file(file, shared_struct_map, shared_dwarf, flags_regex_str).await.unwrap();
+            read_and_process_file(file, shared_struct_map, shared_dwarf, flags_regex_str)
+                .await
+                .unwrap();
             drop(permit);
         });
         handles.push(handle);
@@ -161,41 +173,49 @@ fn byte_offset_to_line_number(content: &Vec<u8>, byte_offset: usize) -> Option<u
         if char == &b'\n' {
             line_number += 1;
         }
-        current_byte_index +=1;
+        current_byte_index += 1;
     }
     None
 }
 
-fn apply_highlight_ranges(content: &mut String, base_range: &std::ops::Range<usize>,
-                          highlight_ranges: &Vec<std::ops::Range<usize>>) {
+fn apply_highlight_ranges(
+    content: &mut String,
+    base_range: &std::ops::Range<usize>,
+    highlight_ranges: &Vec<std::ops::Range<usize>>,
+) {
     let mut added_bytes = 0;
     for range in highlight_ranges {
-        let insert_start = (range.start-base_range.start)+added_bytes;
+        let insert_start = (range.start - base_range.start) + added_bytes;
         content.insert(insert_start, '\x1b');
-        content.insert(insert_start+1, '[');
-        content.insert(insert_start+2, '3');
-        content.insert(insert_start+3, '1');
-        content.insert(insert_start+4, 'm');
+        content.insert(insert_start + 1, '[');
+        content.insert(insert_start + 2, '3');
+        content.insert(insert_start + 3, '1');
+        content.insert(insert_start + 4, 'm');
 
-        let insert_end = (range.end-base_range.start)+added_bytes+5;
+        let insert_end = (range.end - base_range.start) + added_bytes + 5;
         content.insert(insert_end, '\x1b');
-        content.insert(insert_end+1, '[');
-        content.insert(insert_end+2, '0');
-        content.insert(insert_end+3, 'm');
+        content.insert(insert_end + 1, '[');
+        content.insert(insert_end + 2, '0');
+        content.insert(insert_end + 3, 'm');
         added_bytes += 9;
     }
-
 }
 
-fn display_match(content: &Vec<u8>, path: &PathBuf, struct_: &dwat::Struct,
-                 dwarf: &Arc<RwLock<dwat::dwarf::OwnedDwarf>>, qm: &QueryMatch) {
+fn display_match(
+    content: &Vec<u8>,
+    path: &PathBuf,
+    struct_: &dwat::Struct,
+    dwarf: &Arc<RwLock<dwat::dwarf::OwnedDwarf>>,
+    qm: &QueryMatch,
+) {
     let struct_name = qm.struct_name.utf8_text(&content).unwrap();
 
     let dwarf = dwarf.read().expect("failed to aqcuire dwarf rwlock");
     let struct_str = struct_.to_string_verbose(&*dwarf, 1).unwrap();
     drop(dwarf);
 
-    let decl_line_start = byte_offset_to_line_number(&content, qm.function_definition.byte_range().start).unwrap();
+    let decl_line_start =
+        byte_offset_to_line_number(&content, qm.function_definition.byte_range().start).unwrap();
 
     let mut match_ranges: Vec<std::ops::Range<usize>> = vec![];
     match_ranges.push(qm.struct_name.byte_range());
@@ -206,28 +226,34 @@ fn display_match(content: &Vec<u8>, path: &PathBuf, struct_: &dwat::Struct,
 
     let base_range: std::ops::Range<usize> = qm.function_definition.byte_range();
 
-    let mut function_src = qm.function_definition.utf8_text(content).unwrap().to_string();
+    let mut function_src = qm
+        .function_definition
+        .utf8_text(content)
+        .unwrap()
+        .to_string();
 
     // find line index opening brace is on
-    let brace_location = function_src.find('{')
+    let brace_location = function_src
+        .find('{')
         .expect("no opening brace in function source?");
-    let end_decl_line = byte_offset_to_line_number(&content[decl_line_start..].to_vec(), brace_location)
-        .expect("failed when looking for line number of opening brace") - 1;
+    let end_decl_line =
+        byte_offset_to_line_number(&content[decl_line_start..].to_vec(), brace_location)
+            .expect("failed when looking for line number of opening brace")
+            - 1;
 
     // determine which lines will be included, always include lines up to the opening brace
     let mut included_lines: Vec<usize> = (0..end_decl_line).collect();
     let mut seen: usize = 0;
     for (idx, line) in function_src.lines().enumerate() {
         for range in &match_ranges {
-            if (seen..seen+line.len()+1).contains(&(&range.start-&base_range.start)) {
+            if (seen..seen + line.len() + 1).contains(&(&range.start - &base_range.start)) {
                 if !included_lines.contains(&idx) {
                     included_lines.push(idx);
                 }
             }
         }
-        seen += line.len()+1;
+        seen += line.len() + 1;
     }
-
 
     // highlight captures
     if std::io::stdout().is_terminal() {
@@ -240,7 +266,11 @@ fn display_match(content: &Vec<u8>, path: &PathBuf, struct_: &dwat::Struct,
     println!("{}", struct_str);
     println!("");
     if std::io::stdout().is_terminal() {
-        println!("\x1b[1m{}\x1b[0m:{}", path.to_str().unwrap(), decl_line_start);
+        println!(
+            "\x1b[1m{}\x1b[0m:{}",
+            path.to_str().unwrap(),
+            decl_line_start
+        );
     } else {
         println!("{}:{}", path.to_str().unwrap(), decl_line_start);
     }
@@ -248,12 +278,12 @@ fn display_match(content: &Vec<u8>, path: &PathBuf, struct_: &dwat::Struct,
     let src_lines = function_src.lines().collect::<Vec<&str>>();
 
     // add last line if it wasn't already included and it is a return
-    let src_lines_ct = function_src.lines().count()-1;
-    if !included_lines.contains(&(src_lines_ct-1)) {
-        if src_lines[src_lines_ct-1].contains("return ") &&
-            src_lines[src_lines_ct-1].ends_with(";")
+    let src_lines_ct = function_src.lines().count() - 1;
+    if !included_lines.contains(&(src_lines_ct - 1)) {
+        if src_lines[src_lines_ct - 1].contains("return ")
+            && src_lines[src_lines_ct - 1].ends_with(";")
         {
-            included_lines.push(src_lines_ct-1);
+            included_lines.push(src_lines_ct - 1);
         }
     }
     // add last line if it wasn't already included
@@ -283,19 +313,25 @@ struct QueryMatch<'a> {
     assign_name: ts::Node<'a>,
     _assign_call: ts::Node<'a>,
     assign_func: ts::Node<'a>,
-    flags: ts::Node<'a>
+    flags: ts::Node<'a>,
 }
 
-async fn process_file_content(path: PathBuf, content: Vec<u8>, struct_map: Arc<RwLock<HashMap<String, dwat::Struct>>>,
-                              dwarf: Arc<RwLock<dwat::dwarf::OwnedDwarf>>, flags_regex_str: Option<String>)
--> Result<(), Box<dyn std::error::Error>>
-{
+async fn process_file_content(
+    path: PathBuf,
+    content: Vec<u8>,
+    struct_map: Arc<RwLock<HashMap<String, dwat::Struct>>>,
+    dwarf: Arc<RwLock<dwat::dwarf::OwnedDwarf>>,
+    flags_regex_str: Option<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut parser = TsParser::new();
 
-    parser.set_language(ts_c::language())
-          .expect("Error loading C grammar");
+    parser
+        .set_language(ts_c::language())
+        .expect("Error loading C grammar");
 
-    let parsed = parser.parse(&content, None).expect("Parser returned no tree");
+    let parsed = parser
+        .parse(&content, None)
+        .expect("Parser returned no tree");
     let root_node = parsed.root_node();
 
     let query_str = "
@@ -339,7 +375,13 @@ async fn process_file_content(path: PathBuf, content: Vec<u8>, struct_map: Arc<R
 
     for match_ in matches {
         let captures = match_.captures;
-        let struct_name = captures.get(2).unwrap().node.utf8_text(&content).unwrap_or("").to_string();
+        let struct_name = captures
+            .get(2)
+            .unwrap()
+            .node
+            .utf8_text(&content)
+            .unwrap_or("")
+            .to_string();
 
         let struct_map = struct_map.read().unwrap();
         if let Some(struct_) = struct_map.get(&struct_name) {
@@ -347,9 +389,15 @@ async fn process_file_content(path: PathBuf, content: Vec<u8>, struct_map: Arc<R
             if let Some(ref flags_regex_str) = flags_regex_str {
                 flags_regex = Regex::new(&flags_regex_str)?;
             }
-            let flags = captures.get(7).unwrap().node.utf8_text(&content).unwrap_or("").to_string();
+            let flags = captures
+                .get(7)
+                .unwrap()
+                .node
+                .utf8_text(&content)
+                .unwrap_or("")
+                .to_string();
             if flags_regex.find(&flags).is_none() {
-                continue
+                continue;
             }
 
             let qm = QueryMatch {
@@ -369,16 +417,22 @@ async fn process_file_content(path: PathBuf, content: Vec<u8>, struct_map: Arc<R
     Ok(())
 }
 
-async fn read_and_process_file(path: PathBuf, struct_map: Arc<RwLock<HashMap<String, dwat::Struct>>>,
-                               dwarf: Arc<RwLock<dwat::dwarf::OwnedDwarf>>, flags_regex_str: Option<String>)
--> Result<(), Box<dyn std::error::Error>>
-{
+async fn read_and_process_file(
+    path: PathBuf,
+    struct_map: Arc<RwLock<HashMap<String, dwat::Struct>>>,
+    dwarf: Arc<RwLock<dwat::dwarf::OwnedDwarf>>,
+    flags_regex_str: Option<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open(path.clone()).await?;
     let mut contents = vec![];
     file.read_to_end(&mut contents).await?;
 
     let struct_map = struct_map.clone();
     let dwarf = dwarf.clone();
-    let _ = task::spawn_blocking(move || process_file_content(path, contents, struct_map, dwarf, flags_regex_str)).await?.await;
+    let _ = task::spawn_blocking(move || {
+        process_file_content(path, contents, struct_map, dwarf, flags_regex_str)
+    })
+    .await?
+    .await;
     Ok(())
 }
