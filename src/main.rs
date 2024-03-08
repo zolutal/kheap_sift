d1878bdfb3d5c41351d35f4084622965a82a13a9
zolutal <justalmill@gmail.com>
2024-03-08 08:10:39 -0700

    whoops x2

--- src/main.rs
+++ src/main.rs
@@ -294,7 +294,7 @@ fn display_match(
     // set initially to max, so that the elipses won't print the first time through
     let mut last_line = usize::MAX;
     for line_idx in included_lines {
-        if last_line == usize::MAX || last_line + 1 != line_idx {
+        if last_line != usize::MAX && last_line + 1 != line_idx {
             println!("...");
         }
         println!("{}", src_lines[line_idx]);
